8914c604cd4755a52f626dd6270c3e16344eaae4
Andreas Kluge-Kaindl <andi@team33.de>
2014-04-24 09:05:03 +0200

    Initial commit

---
+++ src/main/java/net/team33/imaging/contrast/Main.java
@@ -0,0 +1,11 @@
+package net.team33.imaging.contrast;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    public static void main(final String[] args){
+        System.out.println("Hello world");
+    }
+}
